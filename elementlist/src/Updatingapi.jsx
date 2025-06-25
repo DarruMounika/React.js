0496a768cf5237bcc3af2e88f2bd93b32d12d1ab
DarruMounika <mounikadharru99@gmail.com>
2025-06-25 22:14:15 +0530

    elementlist

--- elementlist/src/Updatingapi.jsx
+++ elementlist/src/Updatingapi.jsx
@@ -4,17 +4,18 @@ import './Updatingapi.css';
 const Updatingapi = () => {
   const [name,setName] = useState("");
   const [age,setAge] = useState("");
+  const [course,setCourse] = useState("");
 
-  const studentDetails = {name,age};
+  const studentDetails = {name,age,course};
 
-  const studentinfo=async(e)=>{
+  const studentinfo = async(e)=>{
     e.preventDefault();
     console.log(studentDetails);
 
-    const response = await fetch("http://127.0.0.1:8000/student/",{
+    const response = await fetch("http://127.0.0.1:8000/api/students/",{
       method:"POST",
       headers: {
-        "Content-Type":"Application/json"
+        "Content-Type":"application/json"
       },
       body: JSON.stringify(studentDetails)
     })
@@ -30,6 +31,8 @@ const Updatingapi = () => {
                 <input type="text" name="name" onChange={(e)=>setName(e.target.value)}/><br />
                 <label> Student age</label><br />
                 <input type="number" name="age" onChange={(e)=>setAge(e.target.value)}/><br />
+                <label>Course</label><br />
+                <input type="text" name="course" onChange={(e)=>setCourse(e.target.value)}/><br />
                 <button className="submitbtn" type="submit">Submit</button>
             </form>
 
